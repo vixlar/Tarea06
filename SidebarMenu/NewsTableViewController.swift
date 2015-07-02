08d00e11e54ea2fa3dbde6f70ccd82650987b69e
vixlar <modelo@iMac-01.local>
2015-07-02 11:07:41 -0500

    Tarea06

---
+++ SidebarMenu/NewsTableViewController.swift
@@ -0,0 +1,119 @@
+//
+//  NewsTableViewController.swift
+//  SidebarMenu
+//
+//  Created by Eduardo Iza on 28/06/15.
+//  Copyright (c) 2015 Unimodelo. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+            // Uncomment to change the width of menu
+            //self.revealViewController().rearViewRevealWidth = 62
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return 3
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! NewsTableViewCell
+
+        // Configure the cell...
+        if indexPath.row == 0 {
+            cell.postImageView.image = UIImage(named: "watchkit-intro")
+            cell.postTitleLabel.text = "Inauguración del Edificio de Salud de la Universidad Modelo Mérida"
+            cell.authorLabel.text = "Universidad Modelo"
+            cell.authorImageView.image = UIImage(named: "appcoda-300")
+
+        } else if indexPath.row == 1 {
+            cell.postImageView.image = UIImage(named: "custom-segue-featured-1024")
+            cell.postTitleLabel.text = "Inauguración del Edificio de Secundaria de la Universidad Modelo Valladolid"
+            cell.authorLabel.text = "Escuela Modelo"
+            cell.authorImageView.image = UIImage(named: "appcoda-300")
+            
+        } else {
+            cell.postImageView.image = UIImage(named: "webkit-featured")
+            cell.postTitleLabel.text = "Inauguración de la Universidad Modelo Chetumal"
+            cell.authorLabel.text = "Universidad Modelo"
+            cell.authorImageView.image = UIImage(named: "appcoda-300")
+            
+        }
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SidebarMenu/PhotoViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PhotoViewController.swift
+//  SidebarMenu
+//
+//  Created by Eduardo Iza on 28/06/15.
+//  Copyright (c) 2015 Unimodelo. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
